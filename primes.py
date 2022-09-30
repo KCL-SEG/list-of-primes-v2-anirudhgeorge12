22b3134fab44cc5241a62fb6f0ad744f934a399f
Anirudh George <anirudh.george@kcl.ac.uk>
2022-09-30 15:39:59 +0100

    Changes in parameter for primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
+def primes(numberOfPrimes):
     list = []
+    n = 2
+    if (numberOfPrimes <= 0):
+        raise ValueError(f"numberOfPrimes = {numberOfPrimes} should be a positive integer.")
+    else:
+        while (len(list) < numberOfPrimes):
+            for i in range(2, n):
+                if n % i == 0:
+                    n += 1
+                    break
+            else:
+                list.append(n)
+                n += 1
+    print(list)
     return list
